8511254cb8508a0d08f86e0e5583706a74a2c3aa
LeenardoLee888 <976260000@qq.com>
2019-08-20 16:50:00 +0800

    QQ群

---
+++ TestDemo.java
@@ -0,0 +1,23 @@
+package com.itheima.demo01_final;
+
+/**
+ * @Author: zhanyingpeng
+ * @Date: 2019/4/27 8:29
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        //1.回顾类的定义
+        //2.回顾对象的创建和使用
+        Dog d1 = new Dog();//使用无参构造创建对象
+        System.out.println(d1.getName());
+        System.out.println(d1.getAge());
+        System.out.println(d1);
+//        Dog d2 = new Dog("旺财",20);//使用有参构造创建对象
+//        System.out.println(d2.getName());
+//        System.out.println(d2.getAge());
+        //3.调用方法
+//        d1.bark();;
+//        d2.bark();
+
+    }
+}
